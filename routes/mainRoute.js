30c6f012032cd9139702a14d4b54ee8311372db4
rajeevmissc <rajeevm@issc.co.in>
2025-07-23 16:46:34 +0530

    Update mainRoute.js

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const userRoute = require('../routes/userRoute')
-const reportRoutes = require('../routes/reportRoutes');
+// const userRoute = require('../routes/userRoute')
+// const reportRoutes = require('../routes/reportRoutes');
 const qboRoutes = require('../routes/qboRoutes');
 const qboauthroutes = require('../routes/qbo');
 const commonRouter = express.Router();
 
-commonRouter.use('/user', userRoute)
-commonRouter.use('/reports', reportRoutes)
+// commonRouter.use('/user', userRoute)
+// commonRouter.use('/reports', reportRoutes)
 commonRouter.use('/qbo', qboRoutes)
 commonRouter.use('/auth/qbo', qboauthroutes)
 module.exports = commonRouter
